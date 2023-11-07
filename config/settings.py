a74f101bd2474f4ce928d5988f4e66e1e416ce33
MrQobiljon <madrahimovq@gmail.com>
2023-11-07 10:33:02 +0500

    books tutorial

--- config/settings.py
+++ config/settings.py
@@ -26,7 +26,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'books.apps.BooksConfig'
+    #tashqi
+    'rest_framework',
+    #local
+    'books.apps.BooksConfig',
+    'api.apps.ApiConfig'
+
 ]
 
 MIDDLEWARE = [
